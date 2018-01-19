acb34b00e13a3bf0538d07c803c0bb2a9d55bd19
Jazmyn <jazmyn.fuller33@qmail.cuny.edu>
2018-01-19 01:22:46 -0500

    Random TargetValue generator works, and score is somewhat working.

--- hitme/hitme/ViewController.swift
+++ hitme/hitme/ViewController.swift
@@ -9,24 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
     
+    var updatedScore : Int = 0
+    var targetLabelNumber = Int(arc4random_uniform(100)+1)
+
+    @IBOutlet weak var targetLabel1: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var sliderLabel: UILabel!
     @IBOutlet weak var label: UILabel!
     @IBAction func slider(_ sender: UISlider) {
         label.text = String(Int(sender.value))
-        if (sliderLabel.text == label.text) {
-            
-        }
-    }
+    } // this is only here for testing, I will delete once I figure out how to do abs value
     
-    // random generator for number to hit needs to go somewhere here. If the target # and slider # are equal player's score increases by 10 points. If the player is within a range of 10 they'll get at least one point. If they are outside of it, they get 0.
-    // score, also increases after it has been determined
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        scoreLabel.text="1"
+        targetLabel1.text = String(targetLabelNumber)
+        scoreLabel.text="0"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -38,7 +37,50 @@ class ViewController: UIViewController {
     @IBAction func reset(_ sender: UIButton) {
         scoreLabel.text="0"
     }
-    @IBAction func information(_ sender: UIButton) {
+    
+    @IBAction func hitMeButtonPressed(_ sender: UIButton) {
+        updateTargetText()
+    }
+    func updateTargetText() {
+        if (sliderLabel.text == String(targetLabelNumber)) {
+            updatedScore = updatedScore + 10
+        }
+        else {
+            if (targetLabelNumber-Int(sliderLabel.text!)!==1) {
+                updatedScore = updatedScore + 9
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==2) {
+                updatedScore = updatedScore + 8
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==3) {
+                updatedScore = updatedScore + 7
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==4) {
+                updatedScore = updatedScore + 6
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==5) {
+                updatedScore = updatedScore + 5
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==6) {
+                updatedScore = updatedScore + 4
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==7) {
+                updatedScore = updatedScore + 3
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==8) {
+                updatedScore = updatedScore + 2
+            }
+            if (targetLabelNumber-Int(sliderLabel.text!)!==9) {
+                updatedScore = updatedScore + 1
+            }
+            else { updatedScore = updatedScore + 0}
+        }
+        scoreLabel.text = String(updatedScore)
+        print(updatedScore)
+        targetLabelNumber = Int(arc4random_uniform(100)+1)
+        targetLabel1.text = String(targetLabelNumber)
+    }
+    func information(_ sender: UIButton) {
        
     }
     
